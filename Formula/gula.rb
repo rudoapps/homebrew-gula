9c3561602974c4b3c805cdd4cad6aa654f5578d8
Fernando Salom <fer@rudo.es>
2024-08-25 21:54:01 +0200

    Update gula.rb

--- Formula/gula.rb
+++ Formula/gula.rb
@@ -1,8 +1,8 @@
 class Gula < Formula
-  desc "Una descripción breve de lo que hace tu script"
-  homepage "https://github.com/tu_usuario/gula"  # Reemplaza con tu URL
-  url "https://github.com/tu_usuario/gula/archive/refs/tags/v1.0.tar.gz"  # URL del tarball de tu versión
-  sha256 "tu_sha256_sum"  # El checksum SHA256 de tu archivo tar.gz
+  desc "Instalador de componentes de gula"
+  homepage "https://github.com/rudoapps/gula"  # Reemplaza con tu URL
+  url "https://github.com/rudoapps/gula/archive/refs/tags/v0.0.1.tar.gz"  # URL del tarball de tu versión
+  sha256 "773b457607040c4e42ef94a0217099535bb23c69b9ad519f969265fd6de2fcf7"  # El checksum SHA256 de tu archivo tar.gz
   license "MIT"  # O la licencia que corresponda
 
   def install
