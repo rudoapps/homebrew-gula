7881e80d86003788b6341f73270be88c92dc792f
Fernando Salom <fer@rudo.es>
2024-08-26 09:18:24 +0200

    Refactor gula script removing extension

--- Formula/gula.rb
+++ Formula/gula.rb
@@ -1,9 +1,9 @@
 class Gula < Formula
   desc "Instalador de componentes de gula"
-  homepage "https://github.com/rudoapps/gula"  # Reemplaza con tu URL
-  url "https://github.com/rudoapps/homebrew-gula/archive/refs/tags/0.0.1.tar.gz"  # URL del tarball de tu versión
-  sha256 "773b457607040c4e42ef94a0217099535bb23c69b9ad519f969265fd6de2fcf7"  # El checksum SHA256 de tu archivo tar.gz
-  license "MIT"  # O la licencia que corresponda
+  homepage "https://github.com/rudoapps/gula"
+  url "https://github.com/rudoapps/homebrew-gula/archive/refs/tags/0.0.2.tar.gz"
+  sha256 "773b457607040c4e42ef94a0217099535bb23c69b9ad519f969265fd6de2fcf7"
+  license "MIT"
 
   def install
     bin.install "gula"
