5d04a380a6e86e38054ceb53bf151ac434f9616c
Fernando Salom <fer@rudo.es>
2024-08-26 09:24:39 +0200

    Change sha256

--- Formula/gula.rb
+++ Formula/gula.rb
@@ -2,7 +2,7 @@ class Gula < Formula
   desc "Instalador de componentes de gula"
   homepage "https://github.com/rudoapps/gula"
   url "https://github.com/rudoapps/homebrew-gula/archive/refs/tags/0.0.2.tar.gz"
-  sha256 "773b457607040c4e42ef94a0217099535bb23c69b9ad519f969265fd6de2fcf7"
+  sha256 "1199b79486bb208830ee28d441cebd206abf4a787ce4dd8ab3d1ce0b372d508b"
   license "MIT"
 
   def install
