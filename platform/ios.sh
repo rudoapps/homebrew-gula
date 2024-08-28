ed76374a52f4a6982fca59e7c3b94c0eb543bd1b
Fernando Salom <fer@rudo.es>
2024-08-28 11:31:30 +0200

    Add ios steps

---
+++ platform/ios.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+install_ios_module() {
+  MODULE_NAME="$1"
+  echo -e "${RED}Pendiente de terminar.${NC}"
+  exit 0  
+}
+
+list_ios() {
+  clone "https://x-token-auth:$KEY@bitbucket.org/rudoapps/gula-ios.git"
+  DIRECTORY_PATH="${TEMPORARY_DIR}/${MODULES_PATH_IOS}"
+  echo -e "${GREEN}Lista de módulos disponibles:"
+  ls -l "$DIRECTORY_PATH" | grep '^d' | awk '{print $9}'
+  echo -e "${NC}"
+  remove_temporary_dir
+}
