78129099229778b337551949a7aec3f1600f8f63
Fernando Salom <fer@rudo.es>
2024-08-29 14:40:08 +0200

    Update gula.rb

--- Formula/gula.rb
+++ Formula/gula.rb
@@ -1,8 +1,8 @@
 class Gula < Formula
   desc "Instalador de componentes de gula"
   homepage "https://github.com/rudoapps/gula"
-  url "https://github.com/rudoapps/homebrew-gula/archive/refs/tags/0.0.10.tar.gz"
-  sha256 "2263563fafea2e9a7a084809d26c1ec7f5bbac1c08fbd454c60c0b0fc100eb12"
+  url "https://github.com/rudoapps/homebrew-gula/archive/refs/tags/0.0.11.tar.gz"
+  sha256 "1ae82cceacd9af33ec12646cd311955a2973af3fc5f741777d3189d6e544f7c6"
   license "MIT"
 
   # Dependencias
