77a8a6d0ccb71c8fc03d7a9f2b99b8d261841a74
Fernando Salom <fer@rudo.es>
2024-08-29 09:32:17 +0200

    Update android steps

--- platform/android.sh
+++ platform/android.sh
@@ -11,38 +11,57 @@ list_android() {
 }
 
 install_android_module() {
-  MODULE_NAME="$1"
-
   # STEP1
   # Verificar si la ruta base existe
-  echo -e "${YELLOW}STEP1 - Localizar package name del proyecto.${NC}"
+  echo -e "${BOLD}-----------------------------------------------${NC}"
+  echo -e "${BOLD}STEP1 - Localizar package name del proyecto.${NC}"
+  echo -e "${BOLD}-----------------------------------------------${NC}"
   detect_package_name
 
   # Clonamos el repositorio a una carpeta temporal
-  echo -e "${YELLOW}STEP2 - Clonación temporal del proyecto de GULA.${NC}"
+  echo -e "${BOLD}-----------------------------------------------${NC}"
+  echo -e "${BOLD}STEP2 - Clonación temporal del proyecto de GULA.${NC}"
+  echo -e "${BOLD}-----------------------------------------------${NC}"
   clone "https://x-token-auth:$KEY@bitbucket.org/rudoapps/gula-android.git"
 
   # Verificar que el módulo existe en el repositorio clonado
-  echo -e "${YELLOW}STEP3 - Verificación de la existencia del módulo: ${MODULE_NAME}.${NC}"
+  echo -e "${BOLD}-----------------------------------------------${NC}"
+  echo -e "${BOLD}STEP3 - Verificación de la existencia del módulo: ${MODULE_NAME}.${NC}"
+  echo -e "${BOLD}-----------------------------------------------${NC}"
   check_module_in_temporary_dir
 
   # Verificar si el módulo ya está instalado en el proyecto destino
-  echo -e "${YELLOW}STEP4 - Verificación instalación previa del módulo: ${MODULE_NAME}.${NC}"
+  echo -e "${BOLD}-----------------------------------------------${NC}"
+  echo -e "${BOLD}STEP4 - Verificación instalación previa del módulo: ${MODULE_NAME}.${NC}"
+  echo -e "${BOLD}-----------------------------------------------${NC}"
   verify_module
   
   # Verificar si la carpeta 'modules' existe; si no, crearla
-  echo -e "${YELLOW}STEP5 - Verificación existencia carpeta: ${MODULE_NAME}.${NC}"
+  echo -e "${BOLD}-----------------------------------------------${NC}"
+  echo -e "${BOLD}STEP5 - Verificación existencia carpeta: ${MODULE_NAME}.${NC}"
+  echo -e "${BOLD}-----------------------------------------------${NC}"
   create_modules_dir
 
   # Copiar el módulo al proyecto destino
-  echo -e "${YELLOW}STEP6 - Copiar ficheros al proyecto.${NC}"
+  echo -e "${BOLD}-----------------------------------------------${NC}"
+  echo -e "${BOLD}STEP6 - Copiar ficheros al proyecto.${NC}"
+  echo -e "${BOLD}-----------------------------------------------${NC}"
   copy_files
 
    # Renombrar los imports en los archivos .java y .kt del módulo copiado
-  echo -e "${YELLOW}STEP7 - Renombrar imports.${NC}"
+  echo -e "${BOLD}-----------------------------------------------${NC}"
+  echo -e "${BOLD}STEP7 - Renombrar imports.${NC}"
+  echo -e "${BOLD}-----------------------------------------------${NC}"
   rename_imports
 
-  echo -e "${YELLOW}STEP8 - Eliminación repositorio temporal.${NC}"
+  echo -e "${BOLD}-----------------------------------------------${NC}"
+  echo -e "${BOLD}STEP8 - Copiar/instalar las dependencias.${NC}"
+  echo -e "${BOLD}-----------------------------------------------${NC}"
+  read_configuration
+
+  echo -e "${BOLD}-----------------------------------------------${NC}"
+  echo -e "${BOLD}STEP9 - Eliminación repositorio temporal.${NC}"
+  echo -e "${BOLD}-----------------------------------------------${NC}"
   remove_temporary_dir
   
   echo -e "${GREEN}-----------------------------------------------${NC}"
