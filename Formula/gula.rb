aa46231d2998bfbffa9bd0f847df778af5ab2d0c
Fernando Salom <fer@rudo.es>
2024-08-30 14:35:24 +0200

    Update gula.rb

--- Formula/gula.rb
+++ Formula/gula.rb
@@ -1,18 +1,18 @@
 class Gula < Formula
   desc "Instalador de componentes de gula"
   homepage "https://github.com/rudoapps/gula"
-  url "https://github.com/rudoapps/homebrew-gula/archive/refs/tags/0.0.16.tar.gz"
+  url "https://github.com/rudoapps/homebrew-gula/archive/refs/tags/0.0.16 .tar.gz"
   sha256 "ef3b98f62bfed5f1b8966c8c5faa2a1d75d871921275f3e747112225ad53c64c"
   license "MIT"
 
   # Dependencias
   # depends_on "ruby" if MacOS.version <= :mojave
+  depends_on "jq" # Añade jq como una dependencia
 
   def install
     # Instalar la gema xcodeproj
     # system "gem", "install", "xcodeproj"
-    depends_on "jq" # Añade jq como una dependencia
-
+    
     bin.install "gula"
     (share/"support/scripts").install "scripts"
   end
