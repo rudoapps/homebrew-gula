27370a4ef12034fae75321e9a7074628cc2ddf7d
Fernando Salom <fer@rudo.es>
2024-08-31 14:36:33 +0200

    Add script to install ios package

--- scripts/ios_support.sh
+++ scripts/ios_support.sh
@@ -1,1 +1,24 @@
 #!/bin/bash
+ios_install_package() {
+	swift package resolve
+	ruby <<RUBY
+	require 'xcodeproj'
+
+	project_path = '$XCODEPROJ_PATH'
+	package_url = '$PACKAGE_URL'
+
+	# Cargar el proyecto Xcode
+	project = Xcodeproj::Project.open(project_path)
+
+	# Crear o encontrar la sección de dependencias
+	main_target = project.targets.first
+	dependencies_group = project.main_group.find_subpath("Dependencies", true)
+
+	# Añadir el paquete Swift
+	main_target.add_package_dependency(url: package_url)
+
+	# Guardar cambios
+	project.save
+	RUBY
+}
+
