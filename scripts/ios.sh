c1d14f16e49dce71db9730a632e97f139166b848
Fernando Salom <fer@rudo.es>
2024-09-02 15:06:41 +0200

    Fix ios script

--- scripts/ios.sh
+++ scripts/ios.sh
@@ -2,12 +2,14 @@
 
 install_ios_module() {
   echo -e "${RED}Pendiente de terminar.${NC}"
-  
+
   os_type=$(check_os)
-  if [ ! $os_type -eq "macOS"]; then
-  	echo -e "${RED}Esta funcionalidada solo puede ser ejecutada en macOS.${NC}"
-  	exit 0
-  exit 0  
+  if [ "$os_type" != "macOS" ]; then
+    echo -e "${RED}Esta funcionalidad solo puede ser ejecutada en macOS.${NC}"
+    exit 0
+  fi
+
+  ios_install_package
 }
 
 list_ios() {
