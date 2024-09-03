d04ec7fdcbd360983a129f1154030cb45b729957
Fernando Salom <fer@rudo.es>
2024-09-03 12:36:41 +0200

    Change success info log

--- scripts/git.sh
+++ scripts/git.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 clone() {
-  remove_temporary_dir
   # Clonar el repositorio específico desde Bitbucket en un directorio temporal
+  remove_temporary_dir
   local repository=$1
   git clone "$repository" --branch main --single-branch --depth 1 "${TEMPORARY_DIR}"
   if [ $? -eq 0 ]; then
-      echo -e "${GREEN}OK.${NC}"
+      echo -e "✅"
   else
     echo -e "${RED}Se ha producido un error descargando el repositorio.${NC}"
     exit 1
