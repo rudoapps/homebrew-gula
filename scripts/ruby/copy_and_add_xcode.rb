8088e012c9422302a2fdc0abeec24b2ff70cd5d4
Fernando Salom <fer@rudo.es>
2024-09-03 23:27:41 +0200

    Add copy and add to xcode script

---
+++ scripts/ruby/copy_and_add_xcode.rb
@@ -0,0 +1,93 @@
+#!/usr/bin/env ruby
+
+require 'xcodeproj'
+require 'fileutils'
+
+def find_xcode_project_and_app_name
+  puts "START"
+  xcode_project_path = Dir.glob("*.xcodeproj").first
+  if xcode_project_path.nil?
+    puts "❌ No se encontró ningún archivo .xcodeproj en el directorio actual."
+    exit 1
+  end
+  app_name = File.basename(xcode_project_path, ".xcodeproj")
+  puts "✅ Proyecto Xcode encontrado: #{xcode_project_path} (Aplicación: #{app_name})"
+  return xcode_project_path, app_name
+end
+
+def copy_all_files(origin, destination)
+  FileUtils.mkdir_p(destination)
+  Dir.glob("#{origin}/**/*").each do |item|
+    relative_path = item.sub("#{origin}/", "")
+    destination_path = File.join(destination, relative_path)
+    if File.directory?(item)
+      FileUtils.mkdir_p(destination_path)
+    else
+      if File.exist?(destination_path)
+        puts "⚠️ El archivo ya existe, omitiendo: #{destination_path}"
+      else
+        FileUtils.mkdir_p(File.dirname(destination_path))
+        FileUtils.cp(item, destination_path)
+      end
+    end
+  end
+end
+
+def create_groups(xcodeproj_path, app_name, destination, destination_relative_path)
+  project = Xcodeproj::Project.open(xcodeproj_path)
+  root_group = project.main_group.find_subpath(app_name, true) || project.main_group.new_group(app_name)
+  groups = destination_relative_path.split('/')
+  current_group = root_group
+
+  groups.each do |group_name|
+    puts group_name
+    current_group = current_group.find_subpath(group_name, true) || current_group.new_group(group_name)
+  end
+
+  Dir.glob("#{destination}/**/*").each do |item|
+    next if File.extname(item) == ".gula"
+    next if File.directory?(item)
+    relative_path = item.sub("#{destination}/", "")
+    path_components = relative_path.split("/")
+    group = current_group
+
+    path_components.each do |component|
+      if File.extname(component).empty?
+        group = group.find_subpath(component, true) || group.new_group(component)
+      end
+    end
+    
+
+    file_name = File.basename(item)
+    
+    #if !File.directory?(item)
+      existing_ref = group.files.find { |f| f.path == item.sub("#{app_name}/", "") }
+      if existing_ref
+        puts "⚠️ El archivo ya forma parte del xcodeproj, omitiendo: #{file_name}"
+      else
+        file_ref = group.new_reference(item.sub("#{app_name}/", ""))
+        puts "✅ Archivo añadido: #{file_name} en el grupo: #{group.name}"
+      end
+    #end
+  end
+  project.save
+end
+
+def main 
+  xcodeproj_path, app_name = find_xcode_project_and_app_name
+
+  origin = ARGV[0]
+  destination_relative_path = ARGV[1]
+  destination = "#{app_name}/#{destination_relative_path}"
+  puts "✅ Copiando desde #{origin} hacia #{destination}"
+  copy_all_files(origin, destination)
+
+  puts "✅ Archivos copiados exitosamente de #{origin} a #{destination}"
+
+  puts "✅ Integrando en el proyecto Xcode: #{xcodeproj_path}"
+  create_groups(xcodeproj_path, app_name, destination, destination_relative_path)
+  
+  puts "✅ Archivos integrados exitosamente en el proyecto Xcode dentro de #{destination_relative_path}"
+end
+
+main
