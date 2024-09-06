ebcdd71182118d4421768df3ce1247212b327a89
Fernando Salom <fer@rudo.es>
2024-09-06 11:53:26 +0200

    Add global vars

---
+++ scripts/global_vars.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+TEMPORARY_DIR="temp-gula"
+MODULE_NAME=""
+KEY=""
+ACCESSTOKEN="" 
+VERSION="0.0.24"
+
+# Definir colores 
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+BOLD='\033[1m'
+NC='\033[0m'
