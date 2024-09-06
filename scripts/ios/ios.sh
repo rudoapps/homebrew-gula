d1b30003dc40b5664deb82940960e22a42292c9e
Fernando Salom <fer@rudo.es>
2024-09-06 11:56:57 +0200

    Add requirements and validation of dependeny

--- scripts/ios/ios.sh
+++ scripts/ios/ios.sh
@@ -2,18 +2,6 @@
 
 MODULES_PATH_IOS="Gula/Modules"
 
-ios_prerequisites() {
-	echo -e "${BOLD}-----------------------------------------------${NC}"
-	echo -e "${BOLD}Prerequisitos: Validando.${NC}"
-	echo -e "${BOLD}-----------------------------------------------${NC}"
-	ACCESSTOKEN=$(get_bitbucket_access_token $KEY ios)
-	if [ $? -eq 0 ]; then
-		echo -e "✅"
-	else
-		echo -e "${RED}Error: No se ha podido completar la validación KEY incorrecta.${NC}"
-		exit 1
-	fi
-}
 
 install_ios_module() {
 	os_type=$(check_os)
@@ -21,55 +9,62 @@ install_ios_module() {
     	echo -e "${RED}Esta funcionalidad solo puede ser ejecutada en macOS.${NC}"
 		exit 0
 	fi
-	ios_prerequisites
+	echo -e "${BOLD}-----------------------------------------------${NC}"
+	echo -e "${BOLD}Prerequisitos: Validando.${NC}"
+	echo -e "${BOLD}-----------------------------------------------${NC}"
+
+	get_access_token $KEY "ios"
 	
 	echo -e "${BOLD}-----------------------------------------------${NC}"
 	echo -e "${BOLD}STEP1 - Clonación temporal del proyecto de GULA.${NC}"
 	echo -e "${BOLD}-----------------------------------------------${NC}"
+
 	clone "https://x-token-auth:$ACCESSTOKEN@bitbucket.org/rudoapps/gula-ios.git"
 
 	echo -e "${BOLD}-----------------------------------------------${NC}"
 	echo -e "${BOLD}STEP2 - Copiar ficheros al proyecto.${NC}"
 	echo -e "${BOLD}-----------------------------------------------${NC}"
 
-
     MODULE_NAME="$(echo "${MODULE_NAME:0:1}" | tr '[:lower:]' '[:upper:]')${MODULE_NAME:1}"
 	DIRECTORY_PATH="${MODULES_PATH_IOS}/${MODULE_NAME}"
 	# ruby "${scripts_dir}/ruby/copy_folder.rb" ${TEMPORARY_DIR} ${DIRECTORY_PATH}
 
-	if ! gem list -i xcodeproj > /dev/null 2>&1; then
-	  echo "La gema 'xcodeproj' no está instalada. Procediendo con la instalación..."
-	  sudo gem install xcodeproj
-	else
-	  echo "La gema 'xcodeproj' ya está instalada."
-	fi
+	echo -e "${BOLD}-----------------------------------------------${NC}"
+	echo -e "${BOLD}STEP3 - Comprobación si esta instalado 'xcodeproj'.${NC}"
+	echo -e "${BOLD}-----------------------------------------------${NC}"
 
-	ruby "${scripts_dir}/ios/ruby/copy_and_add_xcode.rb" "${TEMPORARY_DIR}/${DIRECTORY_PATH}" "Modules/${MODULE_NAME}" "${TEMPORARY_DIR}"
-	if [ $? -eq 0 ]; then
-		echo -e "✅"
-	else
-		echo -e "${RED}Error: No se ha podido copiar el módulo ${MODULE_NAME}.${NC}"
-		exit 1
-	fi
+	ios_check_xcodeproj
+
+	echo -e "${BOLD}-----------------------------------------------${NC}"
+	echo -e "${BOLD}STEP4 - Copiar y añadir a xcode.${NC}"
+	echo -e "${BOLD}-----------------------------------------------${NC}"
+
+	ios_copy_and_add_to_xcode
 
 	echo -e "${GREEN}-----------------------------------------------${NC}"
   	echo -e "${GREEN}Proceso finalizado.${NC}"
   	echo -e "${GREEN}-----------------------------------------------${NC}"
+  	
   	remove_temporary_dir
 }
 
 list_ios() {
-	ios_prerequisites
+	echo -e "${BOLD}-----------------------------------------------${NC}"
+	echo -e "${BOLD}Prerequisitos: Validando.${NC}"
+	echo -e "${BOLD}-----------------------------------------------${NC}"
+
+	get_access_token $KEY "ios"
 	
 	echo -e "${BOLD}-----------------------------------------------${NC}"
 	echo -e "${BOLD}STEP1 - Clonación temporal del proyecto de GULA.${NC}"
 	echo -e "${BOLD}-----------------------------------------------${NC}"
- 	clone "https://x-token-auth:$ACCESSTOKEN@bitbucket.org/rudoapps/gula-ios.git"
 
-	DIRECTORY_PATH="${TEMPORARY_DIR}/${MODULES_PATH_IOS}/"
+ 	clone "https://x-token-auth:$ACCESSTOKEN@bitbucket.org/rudoapps/gula-ios.git"
+	
 	echo -e "${GREEN}Lista de módulos disponibles:"
 	echo -e "${GREEN}-----------------------------------------------"
-	ls -l "$DIRECTORY_PATH" | grep '^d' | awk '{print $9}'  
+	ls -l "${TEMPORARY_DIR}/${MODULES_PATH_IOS}/" | grep '^d' | awk '{print $9}'  
 	echo -e "${GREEN}-----------------------------------------------${NC}"
+
 	remove_temporary_dir
 }
--- scripts/ios/ios_support.sh
+++ scripts/ios/ios_support.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
-prerequisites() {
-	echo -e "${BOLD}-----------------------------------------------${NC}"
-	echo -e "${BOLD}Prerequisitos: Validando.${NC}"
-	echo -e "${BOLD}-----------------------------------------------${NC}"
-	  
-	ACCESSTOKEN=$(get_bitbucket_access_token $KEY flutter)
-	if [ $? -eq 0 ]; then
+ios_check_xcodeproj() {
+	if ! gem list -i xcodeproj > /dev/null 2>&1; then
+	  echo "La gema 'xcodeproj' no está instalada. Procediendo con la instalación..."
+	  sudo gem install xcodeproj
+	  if [ $? -eq 0 ]; then
 		echo -e "✅"
-	else
-		echo -e "${RED}Error: No se ha podido completar la validación KEY incorrecta.${NC}"
+	  else
+		echo -e "${RED}Error: No se ha podido instalar 'xcodeproj' necesaria para continuar ${MODULE_NAME}.${NC}"
 		exit 1
+	  fi
+	else
+	  echo "✅ La gema 'xcodeproj' ya está instalada."
 	fi
 }
 
+ios_copy_and_add_to_xcode() {
+	ruby "${scripts_dir}/ios/ruby/copy_and_add_xcode.rb" "${TEMPORARY_DIR}/${DIRECTORY_PATH}" "Modules/${MODULE_NAME}" "${TEMPORARY_DIR}"
+	if [ $? -eq 0 ]; then
+		echo -e "✅"
+	else
+		echo -e "${RED}Error: No se ha podido copiar el módulo ${MODULE_NAME}.${NC}"
+		exit 1
+	fi
+}
