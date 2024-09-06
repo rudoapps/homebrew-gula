60b8d7bceec72b79b0d783e85f3808e9978db05f
Fernando Salom <fer@rudo.es>
2024-09-06 11:58:23 +0200

    Add new pre requirements steps

--- scripts/flutter/flutter.sh
+++ scripts/flutter/flutter.sh
@@ -2,60 +2,64 @@
 
 MODULES_PATH_FLUTTER="lib/modules"
 
-flutter_prerequisites() {
+list_flutter() {
 	echo -e "${BOLD}-----------------------------------------------${NC}"
 	echo -e "${BOLD}Prerequisitos: Validando.${NC}"
 	echo -e "${BOLD}-----------------------------------------------${NC}"
-	  
-	ACCESSTOKEN=$(get_bitbucket_access_token $KEY flutter)
-	if [ $? -eq 0 ]; then
-		echo -e "✅"
-	else
-		echo -e "${RED}Error: No se ha podido completar la validación KEY incorrecta.${NC}"
-		exit 1
-	fi
-}
 
-list_flutter() {
-	flutter_prerequisites
+	get_access_token $KEY "flutter"
+
 	echo -e "${BOLD}-----------------------------------------------${NC}"
 	echo -e "${BOLD}STEP1 - Clonación temporal del proyecto de GULA.${NC}"
 	echo -e "${BOLD}-----------------------------------------------${NC}"
+
  	clone "https://x-token-auth:$ACCESSTOKEN@bitbucket.org/rudoapps/gula-flutter.git"
 
-	DIRECTORY_PATH="${TEMPORARY_DIR}/${MODULES_PATH_FLUTTER}/"
 	echo -e "${GREEN}Lista de módulos disponibles:"
 	echo -e "${GREEN}-----------------------------------------------"
-	ls -l "$DIRECTORY_PATH" | grep '^d' | awk '{print $9}'  
+	ls -l "${TEMPORARY_DIR}/${MODULES_PATH_FLUTTER}/" | grep '^d' | awk '{print $9}'  
 	echo -e "${GREEN}-----------------------------------------------${NC}"
+
 	remove_temporary_dir
 }
 
 
 install_flutter_module() {	
-	flutter_prerequisites
+	echo -e "${BOLD}-----------------------------------------------${NC}"
+	echo -e "${BOLD}Prerequisitos: Validando KEY.${NC}"
+	echo -e "${BOLD}-----------------------------------------------${NC}"
+
+	get_access_token $KEY "flutter"
+
 	echo -e "${BOLD}-----------------------------------------------${NC}"
 	echo -e "${BOLD}STEP1 - Clonación temporal del proyecto de GULA.${NC}"
 	echo -e "${BOLD}-----------------------------------------------${NC}"
+
 	clone "https://x-token-auth:$ACCESSTOKEN@bitbucket.org/rudoapps/gula-flutter.git"
 
 	echo -e "${BOLD}-----------------------------------------------${NC}"
 	echo -e "${BOLD}STEP2 - Copiar ficheros al proyecto.${NC}"
 	echo -e "${BOLD}-----------------------------------------------${NC}"
+
 	flutter_create_modules_dir
     copy_files "${TEMPORARY_DIR}/lib/modules/${MODULE_NAME}" "lib/modules/"
 
-	if [ $? -eq 0 ]; then
-		echo -e "✅"
-	else
-		echo -e "${RED}Error: No se ha podido copiar el módulo ${MODULE_NAME}.${NC}"
-		exit 1
-	fi
+	echo -e "${BOLD}-----------------------------------------------${NC}"
+	echo -e "${BOLD}STEP3 - Cargando dependencias.${NC}"
+	echo -e "${BOLD}-----------------------------------------------${NC}"
+
 	flutter_read_configuration
+
+	echo -e "${BOLD}-----------------------------------------------${NC}"
+	echo -e "${BOLD}STEP4 - Renombrar imports.${NC}"
+	echo -e "${BOLD}-----------------------------------------------${NC}"
+	
+	flutter_rename_imports
 
 	echo -e "${GREEN}-----------------------------------------------${NC}"
   	echo -e "${GREEN}Proceso finalizado.${NC}"
   	echo -e "${GREEN}-----------------------------------------------${NC}"
+
   	remove_temporary_dir
 }
 
