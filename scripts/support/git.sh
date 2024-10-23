7f206c4839404f81a8c77bb1e470b848426ed6b6
Fernando Salom <fer@rudo.es>
2024-10-23 11:12:31 +0200

    Stop forcing branch to be main

--- scripts/support/git.sh
+++ scripts/support/git.sh
@@ -4,7 +4,7 @@ clone() {
   # Clonar el repositorio específico desde Bitbucket en un directorio temporal
   remove_temporary_dir
   local repository=$1
-  git clone "$repository" --branch main --single-branch --depth 1 "${TEMPORARY_DIR}"
+  git clone "$repository" --single-branch --depth 1 "${TEMPORARY_DIR}"
   if [ $? -eq 0 ]; then
       echo -e "✅ Clonado correctamente en el directorio temporal"
   else
