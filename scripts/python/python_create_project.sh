13cd31625628016b7ba4f1e644aac5241e0b2b4c
Fernando Salom <fer@rudo.es>
2025-08-14 13:18:49 +0200

    Add 2 options for python

--- scripts/python/python_create_project.sh
+++ scripts/python/python_create_project.sh
@@ -1,16 +1,15 @@
 #!/usr/bin/env bash
 
 # Constantes
-ARCH_REPO="bitbucket.org:rudoapps/architecture-python.git"
 execPath=$PWD
 
 
 get_token() {
-    echo "┌──────────────────────────────────────────────"
+    echo ""
+    echo "┌──────────────────────────────────────────────"  
     echo "│"
     echo "│ Validando key"
     echo "│ "
-    echo "└──────────────────────────────────────────────"   
      get_access_token $KEY "back"
 }
 
@@ -24,114 +23,113 @@ helpFun(){
 checkResult(){
     if [ $? != 0 ]
     then
-        echo "┌──────────────────────────────────────────────"
         echo "│"
         echo "│ ❌ Error: Paso '$1' FALLÓ."
         echo "│"
-        echo "└──────────────────────────────────────────────" 
     exit 1
     fi
 }
 
 validate_project_name() {
     if [[ -z "$projectPath" || "$projectPath" =~ ^[[:space:]]*$ ]]; then
-        echo "┌──────────────────────────────────────────────"
         echo "│"
         echo "│ ❌ Error: El nombre del proyecto no puede estar vacío ni ser solo espacios."
         echo "│"
-        echo "└──────────────────────────────────────────────"  
         exit 1
     fi
 
     # Validar caracteres válidos (letras, números, guiones y guiones bajos)
     if [[ ! "$projectPath" =~ ^[a-zA-Z0-9._-]+$ ]]; then
-        echo "┌──────────────────────────────────────────────"
         echo "│"
         echo "│ ❌ Error: El nombre del proyecto contiene caracteres no válidos."
         echo "│ ✅ Usa solo letras, números, guiones (-), guiones bajos (_) y puntos (.)"
         echo "│"
-        echo "└──────────────────────────────────────────────" 
         exit 1
     fi
 }
 
+
 python_create_project() {
     read -p "Introduce la ruta de destino para el nuevo proyecto (por ejemplo, ../NuevaApp): " projectPath
     validate_project_name
+    echo ""
+    set -eu
+    while :; do
+      echo "Elige stack (1-2):"
+      echo "1) fastapi"
+      echo "2) django"
+      printf "> "
+      read ans || exit 1
+
+      case "$ans" in
+        1|fastapi) STACK="fastapi"; break ;;
+        2|django) STACK="django"; break ;;
+        *) echo "Opción inválida. Usa 1/2 o fastapi/django." ;;
+      esac
+    done
+    echo "Stack seleccionado: $STACK"
+
+    case "$STACK" in
+      fastapi)
+        BRANCH="fastapi"
+        ;;
+      django)
+        BRANCH="main"
+        ;;
+    esac
 
     if [ -z "$projectPath" ]; then
-        echo "┌──────────────────────────────────────────────"
         echo "│"
         echo "│ ❌  Error: Faltan parámetros obligatorios."
         echo "│"
-        echo "└──────────────────────────────────────────────"  
         helpFun
         exit 1
     fi
 
     if [ -d "$projectPath" ]; then
-        echo "┌──────────────────────────────────────────────"
         echo "│"
         echo "│ ❌  La carpeta '$projectPath' ya existe. Por seguridad no se sobrescribirá."
-        echo "│"
-        echo "└──────────────────────────────────────────────"        
+        echo "│"   
         exit 1
     fi
 
     # Carpeta temporal
     TEMP_CLONE_DIR="temp-archetype"
     if [ -d "$TEMP_CLONE_DIR" ]; then
-        echo "┌──────────────────────────────────────────────"
         echo "│"
         echo "│ 🗑️  Eliminando carpeta temporal existente: $TEMP_CLONE_DIR"
         echo "│"
-        echo "└──────────────────────────────────────────────"   
         rm -rf "$TEMP_CLONE_DIR"
     fi
     
     get_token
 
-    echo "┌──────────────────────────────────────────────"
     echo "│"
     echo "│ ✅ Clonando arquetipo en carpeta temporal..."
-    echo "│"
-    echo "└──────────────────────────────────────────────"
 
-    git clone "https://x-token-auth:$ACCESSTOKEN@bitbucket.org/rudoapps/architecture-python.git" "$TEMP_CLONE_DIR"
+    git clone --branch "$BRANCH" --depth 1  "https://x-token-auth:$ACCESSTOKEN@bitbucket.org/rudoapps/architecture-python.git" "$TEMP_CLONE_DIR"
     checkResult "Clonando repositorio arquetipo"
 
-    echo "┌──────────────────────────────────────────────"
     echo "│"
     echo "│ ✅ Eliminando .git para limpiar el historial..."
-    echo "│"
-    echo "└──────────────────────────────────────────────"
     rm -rf "$TEMP_CLONE_DIR/.git"
 
-    echo "┌──────────────────────────────────────────────"
     echo "│"
     echo "│ ✅ Copiando contenido en: '$projectPath'..."
-    echo "│"
-    echo "└──────────────────────────────────────────────"
+
 
     mkdir -p "$projectPath"
     cp -R "$TEMP_CLONE_DIR"/. "$projectPath"
     checkResult "Copiando contenido del arquetipo"
 
-    echo "┌──────────────────────────────────────────────"
     echo "│"
     echo "│ ✅ Eliminando carpeta temporal..."
-    echo "│"
-    echo "└──────────────────────────────────────────────"
+
 
     rm -rf "$TEMP_CLONE_DIR"
 
-    echo "┌──────────────────────────────────────────────"
     echo "│"
-    echo "│ Moviéndose a la carpeta del proyecto..."
-     cd "$projectPath"
-    checkResult  "│ Moviéndose a la carpeta del proyecto"
-
-    echo "│ ✅ Ruta absoluta: $(pwd)"
+    echo "│ 👍 Proyecto python preparado en: $(pwd)"
     echo "│"
     echo "└──────────────────────────────────────────────"
    
