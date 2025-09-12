b3b4619b92ec9b74beb14b6bc0e17b369ec0bc5e
Fernando Salom <fer@rudo.es>
2025-09-12 10:45:38 +0200

    Fix problem with required libs

--- Formula/gula.rb
+++ Formula/gula.rb
@@ -8,10 +8,22 @@ class Gula < Formula
   # Dependencias
   depends_on "ruby"
   depends_on "jq" # Añade jq como una dependencia
+  
+  resource "xcodeproj" do
+    url "https://rubygems.org/downloads/xcodeproj-1.23.0.gem"
+    sha256 "6b8663e2ad7b7ff31e4b8a4b998e8c1a8d4a20a7e72c8cebcf62f5f4e2b4e6a5"
+  end
 
   def install
     bin.install "gula"
     (share/"support/scripts").install "scripts"
+    
+    # Instalar gemas Ruby necesarias
+    resources.each do |r|
+      r.stage do
+        system "gem", "install", Dir["*.gem"].first, "--install-dir", libexec
+      end
+    end
   end
 
   def post_install
