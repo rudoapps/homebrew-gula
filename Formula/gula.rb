1e239d66d35e7a935bf2a60e091c6582610b4053
Fernando Salom <fer@rudo.es>
2026-01-08 14:28:35 +0100

    Update Formula to v0.0.156

--- Formula/gula.rb
+++ Formula/gula.rb
@@ -1,8 +1,8 @@
 class Gula < Formula
   desc "CLI para desarrollo móvil y agente IA"
   homepage "https://github.com/rudoapps/gula"
-  url "https://github.com/rudoapps/homebrew-gula/archive/refs/tags/0.0.155.tar.gz"
-  sha256 "9574f57156226ea978b1cf0bd7f40e12a8d1f634a89af3ca345b3a0e40081d84"
+  url "https://github.com/rudoapps/homebrew-gula/archive/refs/tags/0.0.156.tar.gz"
+  sha256 "ec448441bb02a9b7141d514490d94132292c9a1c45e7e41347043e25e8bd2389"
   license "MIT"
 
   depends_on "jq"
