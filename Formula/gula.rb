af0d39fb53bddea410cbe2b510798ead2536cf4c
Fernando Salom <fer@rudo.es>
2026-01-11 10:43:25 +0100

    Update sha256 for v0.0.166

--- Formula/gula.rb
+++ Formula/gula.rb
@@ -2,7 +2,7 @@ class Gula < Formula
   desc "CLI para desarrollo móvil y agente IA"
   homepage "https://github.com/rudoapps/gula"
   url "https://github.com/rudoapps/homebrew-gula/archive/refs/tags/0.0.166.tar.gz"
-  sha256 "PLACEHOLDER_UPDATE_AFTER_PUSH"
+  sha256 "de5f756a6262cc20f73eebf5ca2e88b5790924d5b90e3f2724c0ccdcd8b52475"
   license "MIT"
 
   depends_on "jq"
