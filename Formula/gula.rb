18e39152d13aa3b5e5436a5cb3498cc68c259297
Fernando Salom <fer@rudo.es>
2026-03-18 10:35:56 +0100

    Update formula sha256 for v0.0.244

--- Formula/gula.rb
+++ Formula/gula.rb
@@ -1,8 +1,8 @@
 class Gula < Formula
   desc "CLI para desarrollo móvil"
   homepage "https://github.com/rudoapps/gula"
-  url "https://github.com/rudoapps/homebrew-gula/archive/refs/tags/v0.0.243.tar.gz"
-  sha256 "649ebddf14c6723d4a7078a8f8eede71633a73d40959f03803e4028420ed25f8"
+  url "https://github.com/rudoapps/homebrew-gula/archive/refs/tags/v0.0.244.tar.gz"
+  sha256 "7a5fa6f82579c5f77fb27da0de9297daa560071383148637612d9692557e0d0f"
   license "MIT"
 
   depends_on "go" => :build
